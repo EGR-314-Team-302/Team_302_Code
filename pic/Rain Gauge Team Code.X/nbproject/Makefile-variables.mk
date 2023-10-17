c54a064a55078152c99425d5339955832cd03c44
Kalin Comins <kmcomins@asu.edu>
2023-10-16 22:17:52 -0700

    Adding MPLAB project folder to pic folder

---
+++ pic/Rain Gauge Team Code.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Rain_Gauge_Team_Code.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Rain_Gauge_Team_Code.X.production.hex
